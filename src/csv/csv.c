4f4b13f58ef84271965946b0206fbdb4e863de54
Johannes Thumshirn <morbidrsa@googlemail.com>
2012-01-26 21:08:25 +0100

    Added functionallity to read data from csv file and store in a structure array

---
+++ src/csv/csv.c
@@ -0,0 +1,70 @@
+/* Copyright (c) 2012, Johannes Thumshirn */
+/* See COPYING for Licence details */
+
+#include <string.h>
+#include <errno.h>
+
+#include "csv.h"
+
+bool get_data_from_file(char *fname, struct *data)
+{
+	FILE *f;
+	char tmp[256];
+	char *tt;
+	int col_cnt;
+	int row_cnt;
+	
+	/* Initialize variables */
+	row_cnt = 0;
+	col_cnt = 0;
+
+	/* Open fname for reading */
+	f = fopen(fname, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Could not open datafile %s: %s\n", fname,
+			strerror(errno));
+		return false;
+	}
+
+	/* Allocate memory for 1st row */
+	*data = malloc(sizeof(struct data));
+	if (*data == NULL)
+		goto error;
+		
+	while (!feof(f)) {
+		if (fgets(tmp, sizeof(tmp) - 1, f)) {
+			tt = strtok(tmp, ",");
+			if (row_cnt > 0) {
+				*data = realloc(data,
+						sizeof(struct data) *
+						(row_cnt + 1));
+				if (*data == NULL)
+					goto error;
+			}
+			while (tt != NULL) {
+				switch (col_cnt) {
+				case 0:
+					*data[row_cnt].when = atoi(tt);
+					break;
+				case 1:
+					*data[row_cnt].ammount = atoi(tt);
+					break;
+				}
+				col_cnt++;
+				tt = strtok(NULL, ",");
+			}
+			row_cnt++;
+			col_cnt = 0;
+		}
+	}
+
+	fclose(f);
+
+	return true;
+	
+error:
+	fclose(f);
+	fprintf(stderr, "Could not allocate memory for data structure: %s\n",
+		strerror(errno));
+	return false;	
+}
---
+++ src/csv/csv.h
@@ -0,0 +1,21 @@
+/**
+ * @brief Data structure to hold data
+ *
+ * This data structure holds all data on the events
+ */
+struct data {
+	int when;		/** When did the event occure */
+	int ammount;		/** How often did the event occure */
+};
+
+/**
+ * @brief Get Data from File
+ *
+ * Read in CSV formated file and fill a data struct array
+ *
+ * @param filename 	the filename to read from
+ * @param data		pointer to data structure array the data is stored in
+ *
+ * @return true on success, false on failure
+ */
+bool get_data_from_file(char *filename, struct **data);
